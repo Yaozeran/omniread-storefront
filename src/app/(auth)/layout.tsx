67fa035d996335e62d075492df205c72b1fef847
Yaozeran <zeran.yao@icloud.com>
2026-04-19 23:32:27 +0800

    tidy up

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,6 +7,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "@/app/globals.css";
+
 import AuthProvider from "@/features/auth/context/AuthProvider";
 
 
