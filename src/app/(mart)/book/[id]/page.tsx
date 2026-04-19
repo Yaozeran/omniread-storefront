50273ec6b3454f50d1e2f77900c3f4540c3a79e0
Yaozeran <zeran.yao@icloud.com>
2026-04-19 23:32:06 +0800

    tidy up

--- src/app/(mart)/book/[id]/page.tsx
+++ src/app/(mart)/book/[id]/page.tsx
@@ -3,14 +3,14 @@
  * The book details and sales info page. */
 
 
-import { fetchBookMetadata } from "@/services";
+import { fetchBook } from "@/services";
 
 import ListedBook from "@/features/book/components/ListedBook";
 
 
 async function BookPage({ params }: Readonly<{ params : { id : string } }>) {
 
-  const book = await fetchBookMetadata(params.id);
+  const book = await fetchBook(params.id);
 
   return (
     <main className="w-full px-4 py-6 md:px-6 md:py-8">
--- src/app/(mart)/layout.tsx
+++ src/app/(mart)/layout.tsx
@@ -4,7 +4,6 @@
 
 
 import type { Metadata } from "next";
-
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "@/app/globals.css";
